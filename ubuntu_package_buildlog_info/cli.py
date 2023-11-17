db5c8b8d56cf832a319814dff26ac1f1b54ebec5
Philip Roche <phil.roche@canonical.com>
2023-11-17 13:26:49 +0000

    docs: Add more helpful comments and docstring on what and why we are downloading each file from the build

--- ubuntu_package_buildlog_info/cli.py
+++ ubuntu_package_buildlog_info/cli.py
@@ -46,6 +46,28 @@ def _get_published_sources(archive, version, source_package_name, lp_series, poc
 def get_buildlog_info(
     package_series, package_name, package_version, package_architecture="amd64", ppas=[], lp_user=None
 ):
+    """
+    Get buildlog info for a package in the Ubuntu archive.
+
+    Ubuntu package builds currently do not publish a buildinfo file. However, the buildlog file contains
+    the contents buildinfo file. This script downloads the buildlog file, extracts the buildinfo file and verifies
+    that the buildinfo file is correct based on the checksum in the .changes file.
+
+    Downloads the buildlog, changes files and changelog for a package version in a series and extracts
+    the buildinfofile from the buildlog file.
+
+    It also verifies that the buildinfo file is correct based on the checksum in the .changes file.
+
+    * First we query the Ubuntu archive for the source package version in the specified series and pocket.
+    * If the source package version is not found we query the archive for the binary package version in
+      the specified series and pocket.
+    * If the binary package version is found we get the source package name from the binary package and
+        query the archive for the source package version in the specified series and pocket.
+    * If the source package version is found we download the buildlog, changes file and changelog for the
+        source package version in the specified series and pocket.
+    * We then extract the buildinfo file from the buildlog file.
+    * We then verify that the buildinfo file is correct based on the checksum in the .changes file.
+    """
     if lp_user:
         launchpad = Launchpad.login_with(lp_user, service_root=service_roots["production"], version="devel")
     else:
@@ -81,10 +103,10 @@ def get_buildlog_info(
                     f"INFO: No {package_publication_status} sources found for {package_name} version {package_version} in {package_series} {pocket}"
                 )
                 print("INFO: \tTrying to find a binary package with that name ...")
-                # unable to find published sources for args.package.
+                # unable to find published sources for source package package_name.
                 # Perhaps this is a binary package name so we can
                 # do a lookup to see if there exists a source package for
-                # args.package binary package.
+                # package_name binary package.
                 binaries = _get_binary_packages(
                     archive, package_version, package_name, lp_arch_series, pocket, status=package_publication_status
                 )
@@ -118,34 +140,48 @@ def get_buildlog_info(
                     f"{package_name} version {package_version} in {pocket} pocket with {package_publication_status} sources."
                 )
                 source_package_found = True
+                # we have found the source package, so we can stop iterating over the publication statuses
                 break
         if source_package_found:
+            # if we have found the source package we can stop iterating over the pockets
             break
-    if len(sources) == 1:
 
+    # We now have a source package we can start querying for the buildlog, changes file and changelog
+    if len(sources) == 1:
         builds = sources[0].getBuilds()
         if len(builds) > 1:
+            # we need to find the build for the correct architecture
+            architecture_build_found = False
             for build in builds:
-
                 if build.arch_tag == package_architecture:
-                    _build = builds[0]
-                    source_package_name = _build.source_package_name
-                    source_package_version = _build.source_package_version
-                    source_package_arch_tag = _build.arch_tag
+                    architecture_build_found = True
+                    source_package_name = build.source_package_name
+                    source_package_version = build.source_package_version
+                    source_package_arch_tag = build.arch_tag
 
+                    # Download the changelog file for this build
                     changelog_url = sources[0].changelogUrl()
                     url = launchpad._root_uri.append(urllib.parse.urlparse(changelog_url).path.lstrip("/"))
                     changelog_resp = launchpad._browser.get(url).decode("utf-8")
-                    # write changelog_resp to file named {package_name}.changelog
+                    # write changelog_resp to file named
+                    # {source_package_name}_{source_package_version}_{source_package_arch_tag}.changelog
                     with open(
                         f"{source_package_name}_{source_package_version}_{source_package_arch_tag}.changelog", "w"
                     ) as f:
                         f.write(changelog_resp)
+                        print(
+                            f"INFO: \tchangelog file writen to "
+                            f"{source_package_name}_{source_package_version}_{source_package_arch_tag}.changelog"
+                        )
 
-                    buildlog_url = _build.build_log_url
+                    # Download the build log for this build
+                    buildlog_url = build.build_log_url
                     url = launchpad._root_uri.append(urllib.parse.urlparse(buildlog_url).path.lstrip("/"))
                     buildlog_resp = launchpad._browser.get(url).decode("utf-8")
 
+                    # The build log contains <<PKGBUILDDIR>> which is a placeholder for the actual path to the
+                    # build directory. We need to replace <<PKGBUILDDIR>> with the actual path to the build
+                    # directory so that we can extract an accurate buildinfo file from the buildlog file.
                     # Find line with <<PKGBUILDDIR>> in buildlog_resp and set as pkg_builddir variable
                     pkg_builddir = ""
                     for buildlog_line in buildlog_resp.splitlines():
@@ -156,13 +192,28 @@ def get_buildlog_info(
                             # use regex to extract a directory path from the line eg 'build/apparmor-BXxSs1/apparmor-3.0.4'
                             pkg_builddir_regex = re.compile(r"'build/.*' ")
                             pkg_builddir = pkg_builddir_regex.search(buildlog_line).group(0).replace("'", "")
+                            # trim all whitepace from pkg_builddir
+                            pkg_builddir = pkg_builddir.strip()
+                            # we have found the line with <<PKGBUILDDIR>> so we can stop iterating over the lines
+                            # in the buildlog
                             break
 
+                    # write the build log to file named
+                    # {source_package_name}_{source_package_version}_{source_package_arch_tag}.buildlog
                     with open(
                         f"{source_package_name}_{source_package_version}_{source_package_arch_tag}.buildlog", "w"
                     ) as f:
                         f.write(buildlog_resp)
+                        print(
+                            f"INFO: \tbuildlog writen to "
+                            f"{source_package_name}_{source_package_version}_{source_package_arch_tag}.buildlog"
+                        )
 
+                    # The buildinfo file is contained between the lines
+                    # | Buildinfo                                                                    |
+                    # and
+                    # | Package contents                                                             |
+                    # in the build log.
                     buildinfo_start = "| Buildinfo                                                                    |"
                     buildinfo_end = "| Package contents                                                             |"
                     buildlog_header_separator = (
@@ -175,6 +226,7 @@ def get_buildlog_info(
                             append_to_buildinfo = True
 
                         if buildinfo_end in buildlog_line:
+                            # we have reached the end of the buildinfo section so we can stop iterating over the lines
                             break
                         if (
                             append_to_buildinfo
@@ -185,8 +237,6 @@ def get_buildlog_info(
                         ):
                             buildinfo = "{}{}\n".format(buildinfo, buildlog_line)
 
-                    # trim all whitepace from pkg_builddir
-                    pkg_builddir = pkg_builddir.strip()
                     # replace <<PKGBUILDDIR>> in buildinfo with the actual path
                     buildinfo = buildinfo.replace("<<PKGBUILDDIR>>", pkg_builddir)
 
@@ -195,8 +245,12 @@ def get_buildlog_info(
                     )
                     with open(buildinfo_filename, "w") as f:
                         f.write(buildinfo)
+                        print(
+                            f"INFO: \tbuildinfo file writen to "
+                            f"{source_package_name}_{source_package_version}_{source_package_arch_tag}.buildinfo"
+                        )
 
-                    changesfile_url = _build.changesfile_url
+                    changesfile_url = build.changesfile_url
                     url = launchpad._root_uri.append(urllib.parse.urlparse(changesfile_url).path.lstrip("/"))
                     changesfile_resp = launchpad._browser.get(url).decode("utf-8")
 
@@ -216,13 +270,23 @@ def get_buildlog_info(
                                 print(f"INFO: \tHash of {buildinfo_filename} matches hash in changes file.")
                             else:
                                 print(f"INFO: \tHash of {buildinfo_filename} does not match hash in changes file.")
+                            # we have found the hash of the buildinfo_filename in the changes file so we can stop
+                            # iterating over the changesfile lines
                             break
 
+                    # write changesfile_resp to file named
+                    # {source_package_name}_{source_package_version}_{source_package_arch_tag}.changes
                     with open(
                         f"{source_package_name}_{source_package_version}_{source_package_arch_tag}.changes", "w"
                     ) as f:
                         f.write(changesfile_resp)
+                        print(
+                            f"INFO: \tchanges file writen to {source_package_name}_{source_package_version}_{source_package_arch_tag}.changes"
+                        )
 
+                if architecture_build_found:
+                    # if we have found the build for the correct architecture we can stop iterating over the builds
+                    break
         else:
             print(f"Unable to find builds for package {package_name} version {package_version}")
     else:
